a19fd10e50d75a09e41e3eb6b86b3ebec63793a6
nanaynaomi <81824415+grantnaomi@users.noreply.github.com>
2022-03-06 17:17:53 -0800

    remove some comments

--- slackbot/game.py
+++ slackbot/game.py
@@ -1,13 +1,4 @@
-# LATER THIS WILL BE SPLIT UP INTO DIFFERENT FILES:
 from rooms import *
 from items import *
 from commands import *
 from start_game import *
-
-
-# ROOMS ---------------------------------------------
-
-# ITEMS ---------------------------------------------
-
-# COMMANDS ------------------------------------------
-
